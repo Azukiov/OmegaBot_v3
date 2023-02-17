ab012ebd79a971301b30bcc689d45d4b345d4800
Its_Azukio <augustin.pro.prs@gmail.com>
2023-02-17 14:56:04 +0100

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,22 @@
+const { InteractionType } = require('discord.js');
+const { readdirSync } = require('fs');
+
+module.exports = {
+    name: 'interactionCreate',
+
+    async execute(interaction) {
+        let client = interaction.client;
+
+        if (interaction.type == InteractionType.ApplicationCommand) {
+            if (interaction.user.bot) return;
+
+            readdirSync('./src/commands').forEach(file => {
+                const command = require(`../../src/commands/${file}`);
+
+                if (interaction.commandName.toLowerCase() === command.data.name.toLowerCase()) {
+                    command.run(client, interaction)
+                }
+            })
+        }
+    }
+}
